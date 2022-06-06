166c6f8fd333668267e388cd6c204c9dd939d6b0
ck996 <ck996>
2022-06-06 10:09:53 +0800

    clicked point

--- 001_图形化/009_webGL/0005_ClickedPoint/index.js
+++ 001_图形化/009_webGL/0005_ClickedPoint/index.js
@@ -1,10 +1,9 @@
 // 顶点着色器
 const VSHADER_SOURCE = `
     attribute vec4 a_Position;
-    attribute float a_PointSize;
     void main (){
         gl_Position=a_Position; // 设置坐标
-        gl_PointSize=a_PointSize; // 设置尺寸
+        gl_PointSize=10.0; // 设置尺寸
     }
 `
 // 片元着色器
@@ -42,20 +41,36 @@ function main() {
     // 将顶点文职传输给attribute 变量
     gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0)
 
-    const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize')
-    if (a_PointSize < 0) {
-        console.log('Failed to get the storage location of a_PointSize')
-        return false
-    }
-    // 设置大小
-    gl.vertexAttrib1f(a_PointSize, 35.0)
-
     // 指定清空 canvas 的颜色
     gl.clearColor(0.0, 0.0, 0.0, 1.0)
 
+    // 注册鼠标点击事件
+    canvas.onmousedown = function (ev) {
+        click(ev, gl, canvas, a_Position)
+    }
     // 清空 canvas
     gl.clear(gl.COLOR_BUFFER_BIT)
 
-    // 绘制一个点
-    gl.drawArrays(gl.POINTS, 0, 1)
+
+
+}
+let g_points = []
+function click(ev, gl, canvas, a_Position) {
+    let x = ev.clientX;
+    let y = ev.clientY;
+    const rect = ev.target.getBoundingClientRect()
+    x = ((x - rect.left) - canvas.height / 2) / (canvas.height / 2)
+    y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2)
+    g_points.push(x)
+    g_points.push(y)
+    //clear canvas
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    let len = g_points.length
+    for (let i = 0; i < len; i += 2) {
+        // 将点的位置传递到变量 a_Position 中
+
+        gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0)
+        gl.drawArrays(gl.POINTS, 0, 1)
+
+    }
 }
