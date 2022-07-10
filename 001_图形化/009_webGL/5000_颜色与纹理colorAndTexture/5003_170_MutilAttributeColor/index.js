cd58752e6d6c4427733866e779b4b340800f6e2c
ck996 <ck996>
2022-07-10 14:04:34 +0800

    5002

--- 001_图形化/009_webGL/5000_颜色与纹理colorAndTexture/5003_170_MutilAttributeColor/index.js
+++ 001_图形化/009_webGL/5000_颜色与纹理colorAndTexture/5003_170_MutilAttributeColor/index.js
@@ -2,16 +2,22 @@
 const VSHADER_SOURCE = `
     attribute vec4 a_Position;
     attribute float a_PointSize;
+    attribute vec4 a_Color;    
+    varying vec4 v_Color; 
     void main (){
         gl_Position=a_Position; // 设置坐标
-        gl_PointSize=a_PointSize; // 设置尺寸
+        gl_PointSize=20.0; // 设置尺寸
+        v_Color = a_Color;
     }
 `
 
 // 片元着色器
 const FSHADER_SOURCE = `
+    // GLSL的精度限定符 https://www.jianshu.com/p/0e3f80467d46
+    precision mediump float;
+    varying  vec4 v_Color;
     void main(){
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor = v_Color;
     }
 `
 
@@ -46,37 +52,40 @@ function main() {
     gl.clear(gl.COLOR_BUFFER_BIT)
 
     // 绘制
-    gl.drawArrays(gl.POINTS, 0, n)
+    // gl.drawArrays(gl.POINTS, 0, n)
+    gl.drawArrays(gl.TRIANGLES, 0, n)
 }
 
 function initVertexBuffers(gl) {
-    const verticesSizes = new Float32Array([
-        0.0, 0.5, 10.0,
-        -0.5, -0.5, 20.0,
-        0.5, -0.5, 30.0,
+    const verticesColors = new Float32Array([
+        // 顶点坐标和颜色
+        0.0, 0.5, 1.0, 0.0, 0.0,
+        -0.5, -0.5, 0.0, 1.0, 0.0,
+        0.5, -0.5, 0.0, 0.0, 1.0,
     ])
+    // 顶点数量
     let n = 3
 
     // 创建缓存区对象
-    let vertexSizeBuffer = gl.createBuffer()
+    let vertexColorBuffer = gl.createBuffer()
 
     // 将顶点坐标写入缓存区对象并开启
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexSizeBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer)
     // 向缓存区写入对象写入数据
-    gl.bufferData(gl.ARRAY_BUFFER, verticesSizes, gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW)
 
     // BYTES_PER_ELEMENT 每个元素所占的字节数
-    let FSIZE = verticesSizes.BYTES_PER_ELEMENT
+    let FSIZE = verticesColors.BYTES_PER_ELEMENT
     let a_Position = gl.getAttribLocation(gl.program, 'a_Position')
     // 将缓存区对象分配给 a_Position 变量
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false ,FSIZE * 3, 0)
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0)
     // 链接a_Position 变量与分配给他的缓存区对象
     gl.enableVertexAttribArray(a_Position) // 开启分配
 
     // 将顶点尺寸写入缓存区对象并开启
-    let a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize')
+    let a_Color = gl.getAttribLocation(gl.program, 'a_Color')
     //                                               归一化  向量值      偏移量
-    gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, FSIZE * 3, FSIZE * 2)
-    gl.enableVertexAttribArray(a_PointSize) // 开始缓存区分配
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2)
+    gl.enableVertexAttribArray(a_Color) // 开始缓存区分配
     return n
 }
