5a05502218ae2462bcb238ca78f7860956b9b34e
ck996 <ck996>
2022-07-10 20:57:24 +0800

    5005

---
+++ 001_图形化/009_webGL/5000_颜色与纹理colorAndTexture/5005_187_TexTuredQuad/index.js
@@ -0,0 +1,143 @@
+// 顶点着色器
+const VSHADER_SOURCE = `
+    attribute vec4 a_Position;
+    attribute float a_PointSize;
+    attribute vec2 a_TexCoord;
+    varying vec2 v_TexCoord; 
+    void main (){
+        gl_Position=a_Position; // 设置坐标
+        gl_PointSize=20.0; // 设置尺寸
+        v_TexCoord = a_TexCoord;
+    }
+`
+
+// 片元着色器
+const FSHADER_SOURCE = `
+    // GLSL的精度限定符 https://www.jianshu.com/p/0e3f80467d46
+    precision mediump float;
+    uniform sampler2D u_Sampler; 
+    varying  vec2 v_TexCoord;
+   
+    void main(){
+        gl_FragColor = texture2D(u_Sampler, v_TexCoord);
+    }
+`
+
+
+function main() {
+    const canvas = document.getElementById('webgl')
+    if (!canvas) {
+        console.log('Failed to retrieve the canvas element')
+        return false;
+    }
+    let gl = canvas.getContext('webgl')
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL')
+        return false
+    }
+
+    // 初始化着色器
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders')
+        return false
+    }
+    // 设置顶点信息
+    const n = initVertexBuffers(gl)
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices')
+        return false
+    }
+
+    // 设置纹理
+    if (!initTexTrues(gl, n)) {
+
+    }
+
+    // 指定清空 canvas 的颜色
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+    // 清空 canvas
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // 绘制
+    // gl.drawArrays(gl.POINTS, 0, n)
+    gl.drawArrays(gl.TRIANGLES, 0, n)
+}
+
+function initVertexBuffers(gl) {
+    // const vertexTexCoord = new Float32Array([
+    //     // 顶点坐标和 纹理坐标
+    //     -0.5, 0.5, 0.0, 1.0,
+    //     -0.5, -0.5, 0.0, 0.0,
+    //     0.5, 0.5, 1.0, 1.0,
+    //     0.5, -0.5, 1.0, 0.0,
+    // ])
+    const vertexTexCoord = new Float32Array([
+        // 顶点坐标和 纹理坐标
+        -0.5, 0.5, -0.3, 1.7,
+        -0.5, -0.5, -0.3, -0.2,
+        0.5, 0.5, 1.7, 1.7,
+        0.5, -0.5, 1.7, -0.2,
+    ])
+    // 顶点数量
+    let n = 4
+
+    // 创建缓存区对象
+    let vertexTexCoordBuffer = gl.createBuffer()
+
+    // 将顶点坐标和纹理坐标写入缓冲区对象
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer)
+    // 向缓存区写入对象写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertexTexCoord, gl.STATIC_DRAW)
+
+    // BYTES_PER_ELEMENT 每个元素所占的字节数
+    let FSIZE = vertexTexCoord.BYTES_PER_ELEMENT
+    let a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+    // 将缓存区对象分配给 a_Position 变量
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0)
+    // 链接a_Position 变量与分配给他的缓存区对象
+    gl.enableVertexAttribArray(a_Position) // 开启分配
+
+
+    // 将顶点尺寸写入缓存区对象并开启
+    let a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord')
+    //                                               归一化  向量值      偏移量
+    gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2)
+    gl.enableVertexAttribArray(a_TexCoord) // 开始缓存区分配
+    return n
+}
+
+function initTexTrues(gl, n) {
+    // 创建纹理对象
+    let texture = gl.createTexture()
+    // 获取 u_samplaer 存储位置
+    let u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler')
+    // 创建一个image 对象
+    let image = new Image()
+    image.onload = function (){
+        loadTexture(gl,n, texture, u_Sampler, image)
+    }
+    image.src = "../../WebGL-Programming-Guide/texture/mud_cracked_dry_03_diff_1k.jpg"
+    return true;
+}
+function loadTexture(gl,n, texture, u_Sampler, image){
+    // 由于图像的坐标系统的 y 轴 和webgl 的坐标系统是相反的 所以需要对齐进行y轴反转
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1) // 对纹理图像进行 y 轴反转
+
+    // 开启 0 号纹理单元  必须绑定纹理, 默认有 8 个纹理单元
+    gl.activeTexture(gl.TEXTURE0)
+
+    // 向target 绑定纹理对象 TEXTURE_2D 二维纹理 TEXTURE_CUBE_MAP 立方体纹理
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+
+    // 配置纹理参数
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+
+    // 配置纹理图像
+    gl.texImage2D(gl.TEXTURE_2D,0,gl.RGB,gl.RGB, gl.UNSIGNED_BYTE, image)
+
+    // 将0号纹理传递给着色器
+    gl.uniform1i(u_Sampler, 0 )
+    // 绘制矩形
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n)
+}
--- 001_图形化/009_webGL/doc_0003_gl内置函数.md
+++ 001_图形化/009_webGL/doc_0003_gl内置函数.md
@@ -7,3 +7,34 @@
 
 ## gl.vertexAttribPointer
 ![](WebGL-Programming-Guide/images/img_6.png)
+
+## gl.createTexture() 创建纹理对象
+## gl.deleteTexture(texture) 删除纹理对象
+
+
+## gl.texParameteri()
+![](WebGL-Programming-Guide/images/img_7.png)
+pname: 默认值: param
+放大: gl.TEXTURE_MAG_FILTER 默认值  gl.LINEAR
+缩小: gl.TEXTURE_MIN_FILTER        gl.NEAREST_MIPMAP_LINEAR
+水平填充: gl.TEXTURE_WRAP_S         gl.REPEAT
+垂直填充: gl.TEXTURE_WRAP_T         gl.REPAET
+
+可以赋值给gl.TEXTURE_MAG_FILTER 和 gl.TEXTURE_MIN_FILTER的非金字塔纹理类型常量
+gl.NEAREST
+使用原纹理上的距离映射后像素(新像素) 中心最近的那个像素的颜色值,作为新像素的值(使用曼哈顿距离)
+
+gl.LINEAR 使用距离新像素中心最近的是个像素点的颜色值加权平均, 作为新像素的值(与gl.NEAPEST 相比,该方法图像效果更好, 但是会有较大的开销)
+
+可以赋值给gl.TEXTURE_S 和 gl.TEXTURE_WARP_T 的常量
+gl.REPEAT 平铺式的重复纹理
+gl.MIRRORED_REPEAT 镜像对称式的重复纹理
+gl.CLAMP_TO_EDGE 使用纹理图像边缘值
+
+纹素数据的格式
+gl.RGB 红绿蓝
+gl.RGBA 红绿蓝透明度
+gl.ALPHA (0.0,0.0,0.0, 透明度)
+gl.LUMINANCE L,L,L,1L: 流明
+gl.LUMINANCE_ALPHA L,L,L, 透明度
+这里的流明(luminance) 表示我们感知到的物体表面的亮度, 通常使用物体的表面的红绿蓝颜色的分量值的加权平均来计算流明
